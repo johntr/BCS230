dac4e9a24332150795f182880bb0a6002c037cf2
John Redlich <john@John-Redlichs-Macbook-Pro.local>
2013-10-31 18:21:30 -0400

    Initial Commit

---
+++ elevator/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  elevator
+//
+//  Created by John Redlich on 10/31/13.
+//  Copyright (c) 2013 John Redlich. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
