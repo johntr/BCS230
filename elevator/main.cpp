09845c56c8b54c2f5b865684a4cd13cb1f5b55ee
John Redlich <john@John-Redlichs-Macbook-Pro.local>
2013-10-31 19:02:39 -0400

    This is a program to do simple elevator requests. Note: pass the amount of seconds you would like the elevator to wait to the wait method and the floor requested to the request method.

--- elevator/main.cpp
+++ elevator/main.cpp
@@ -1,18 +1,71 @@
-//
-//  main.cpp
-//  elevator
-//
-//  Created by John Redlich on 10/31/13.
-//  Copyright (c) 2013 John Redlich. All rights reserved.
-//
-
 #include <iostream>
+#include <ctime>
+using namespace std;
 
-int main(int argc, const char * argv[])
-{
+class Elevator{
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+private:
+    
+    
+public:
+    int currentFloor;
+    Elevator();
+    void request(int);
+    void wait(int);
+    
+};
+
+Elevator::Elevator() {
+    currentFloor = 1;
 }
 
+
+void Elevator::request(int newFloor){
+    
+    if(newFloor == currentFloor){
+        wait(5);
+        cout << "waiting another 5 seconds on floor " << currentFloor <<endl;
+    }
+    else if(newFloor > currentFloor){
+        cout << "starting at  floor " << currentFloor << endl;
+        while (newFloor > currentFloor) {
+            currentFloor ++;
+            cout << "Going up now to floor ";
+            wait(2);
+            cout << currentFloor << endl;
+            
+            }
+        cout << "Stopped at floor " << currentFloor << endl;
+        wait(2);
+       }
+    //going down
+    else if (newFloor < currentFloor){
+        cout << "starting at  floor " << currentFloor << endl;
+        while (newFloor < currentFloor) {
+            currentFloor --;
+            cout << "Going down to floor ";
+            wait(2);
+            cout << currentFloor << endl;
+            
+        }
+        cout << "Stopped at floor " << currentFloor << endl;
+        wait(2);
+    }
+}
+
+void Elevator::wait(int t){
+    int start = time(0), finish = 0;
+    while (finish - start < t)
+        finish = time(0);
+}
+
+int main() {
+    Elevator go;
+    
+    go.request(5);
+    go.request(3);
+    go.request(3);
+    
+    return 0;
+    
+}
